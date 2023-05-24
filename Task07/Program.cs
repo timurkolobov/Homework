06880c4a24bfbd65f835b2fc24eed60d862c1b9a
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-05-24 16:17:31 +0300

    Added Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"{lastDigit} является последней цифрой числа {number}.");
+}
+else
+{
+    Console.WriteLine("Неверное число.");
+}
