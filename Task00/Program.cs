cc20e14d6e98651f816c72f9d822cbb306d91e8a
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-05-24 15:38:30 +0300

    Added Task01

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // 0. Демонстрация решения
+// Напишите программу, которая на вход принимает число и выдаёт его квадрат(число
+// умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9
+// -7 -> 49 
+
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num == num1 * num1)
+{
+    Console.WriteLine($"Да, {num} является квадратом числа{num1}");
+}
+else Console.WriteLine($"Нет, {num} не является квадратом числа {num1}");
