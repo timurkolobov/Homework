65225180532161674e47c6cd0bf630a5f75e66e5
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-05-24 15:54:02 +0300

    Added Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число, соответствующее дню недели ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1 && num <= 7)
+{
+    if (num ==1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    else if (num == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    else if (num == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+     else if (num == 4)
+     {
+        Console.WriteLine("Четверг");
+     }
+     else if (num == 5)
+     {
+        Console.WriteLine("Пятница");
+     }
+     else if (num == 6)
+     {
+        Console.WriteLine("Суббота");
+     }
+     else if (num == 7)
+     {
+        Console.WriteLine("Воскресенье");
+     }
+else 
+{
+    Console.WriteLine("Введено неправильное число. ");
+}
+}
