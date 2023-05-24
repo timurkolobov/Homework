67bcc0f68f1a2e4390c0b2e00f38150e02346725
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-05-24 16:06:44 +0300

    Added Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = -number;
+while(i <= number)
+{
+    Console.Write($"{i} ");
+    ++i;
+}
