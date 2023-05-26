485d382ae7fe33062b0e3375991512cc1d852555
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-05-26 23:35:58 +0300

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Таблица кубов чисел от 1 до " + n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i + "^3=" + Math.Pow(i, 3));
+        }
+    }
+}
+
