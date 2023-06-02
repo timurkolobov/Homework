3f38f7f09fb5b15ab018405cd40dc98cadfd1ccd
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-02 22:34:23 +0300

    Added Task31

---
+++ Task31/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна
+// -20.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(min, max + 1);
+}
+return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+for (int i = 0; i < arr.Length; i++)
+{
+if (i < arr.Length - 1) Console.Write ($"{arr[i]}{sep} ");
+else Console.Write ($"{arr[i]}");
+}
+}
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
