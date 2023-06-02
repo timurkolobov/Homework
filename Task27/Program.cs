a1d3ed7d83070e879f74047db9d551b9e242db23
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-02 22:14:20 +0300

    Added Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;
+
+public class Program {
+  public static void Main(string[] args) {
+    Console.WriteLine("Введите число: "); // запрос на ввод числа
+    int number = int.Parse(Console.ReadLine()); // считываем число
+    int sum = 0;
+    while (number != 0) {
+        int digit = number % 10; // получаем последнюю цифру числа
+        sum += digit; // добавляем её к сумме цифр
+        number /= 10; // отбрасываем последнюю цифру числа
+    }
+    Console.WriteLine("Сумма цифр в числе: " + sum); // выводим результат
+  }
+}
