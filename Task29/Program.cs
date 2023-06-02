814f6b94066085b5c6cdcbd5c411debe8e56803a
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-02 22:20:01 +0300

    Added Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+using System;
+
+class Program {
+    static void Main() {
+        int[] arr1 = new int[] {1, 2, 5, 7, 19}; // создаем массив arr1
+        int[] arr2 = new int[] {6, 1, 33}; // создаем массив arr2
+        
+        PrintArray(arr1); // выводим arr1 на экран
+        PrintArray(arr2); // выводим arr2 на экран
+    }
+
+    static void PrintArray(int[] arr) {
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++) {
+            if (i == arr.Length - 1) {
+                Console.Write(arr[i]);
+            } else {
+                Console.Write(arr[i] + ", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
