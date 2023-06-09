13a29b86a8faf7e19a27e91aa77bc47a521c9a28
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-09 13:43:40 +0300

    Added Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите числа через запятую: ");
+        string inputStr = Console.ReadLine();
+        string[] inputArr = inputStr.Split(',');
+        int count = 0;
+
+        foreach (string number in inputArr)
+        {
+            int parsedNumber = int.Parse(number.Trim());
+            
+            if (parsedNumber > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел, больших 0: {count}");
+        Console.ReadLine();
+    }
+}
