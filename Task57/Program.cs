c3c0dead7aaf7569398af739e3841779c67b758a
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-14 23:35:39 +0300

    Added Task57

---
+++ Task57/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+
+
+void CountFrequencies(int[] array)
+{
+int currentNumber = array[0];
+int count = 1;
+for (int i = 1; i < array.Length; i++)
+{
+if (array[i] == currentNumber) count++;
+else
+{
+Console.WriteLine($"Число {currentNumber} встречается {count} раз");
+currentNumber = array[i];
+count = 1;
+}
+}
+Console.WriteLine($"Число {currentNumber} встречается {count} раз");
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 9);
+int[] array = MatrixToArray(array2d);
+Array.Sort(array);
+
+PrintMatrix(array2d);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+CountFrequencies(array);
