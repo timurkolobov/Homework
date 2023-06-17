43e7e698fe8e2893b414a17a8c0480bc850dca57
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-17 11:41:58 +0300

    Added Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        while (num > 0) 
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        Console.WriteLine($"Сумма цифр числа: {sum}");
+    }
+}
