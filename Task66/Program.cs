a112d3f7224b2fd0d7ffefc1be41b6d4bcc95250
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-17 12:01:40 +0300

    Added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число M: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите число N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = Sum(m, n); // вызов рекурсивной функции
+
+            Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}");
+        }
+
+        static int Sum(int m, int n)
+        {
+            if (m == n)
+            {
+                return m; // базовый случай
+            }
+            else
+            {
+                return m + Sum(m + 1, n); // рекурсивный случай
+            }
+        }
+    }
+}
