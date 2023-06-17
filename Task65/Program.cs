3cb9aeb6f66ce6826301827ad1b568e0db05c45b
timurkolobov <131962157+timurkolobov@users.noreply.github.com>
2023-06-17 11:32:50 +0300

    Added Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void ConsecutiveNumbersInRange(int num1, int num2)
+{
+
+if (num1 < num2)
+{
+Console.Write($"{num1}, ");
+ConsecutiveNumbersInRange(num1 + 1, num2);
+}
+else if (num2 < num1)
+{
+Console.Write($"{num2}, ");
+ConsecutiveNumbersInRange(num1, num2 + 1);
+}
+else
+{
+Console.WriteLine($"{num1}");
+}
+}
+
+Console.WriteLine("Введите два натуральных числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+ConsecutiveNumbersInRange(number1, number2);
